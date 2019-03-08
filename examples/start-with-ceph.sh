157b95a4de167bc20ec8bee83fdb0094443ff422
林英杰 <linyingjie@ascs.tech>
2019-03-08 09:29:05 +0800

    feature: 增加了mesos-marathon 平台的用例

--- examples/start-with-ceph.sh
+++ examples/start-with-ceph.sh
@@ -3,21 +3,16 @@ docker stop ceph-starter
 docker rm   ceph-starter
 
 #启动一个临时测试的web服务器
-cd ..
 ls -al helloworld.tar.gz
-#current_file_path=$(cd "$(dirname "$0")"; pwd)
-#filesource="file://${current_file_path}/helloworld.tar.gz"
-
-#-v `pwd`/helloworld.tar.gz:/server/data/helloworld.tar.gz \
-
 echo "需要安装rexray服务 需要把helloworld.tar.gz 拷贝到 ceph 存储指定位置上."
 
 docker run -d --name ceph-starter \
---add-host download.cityworks.cn:192.168.66.36 \
 --volume-driver rexray \
--v helloworld-data:/server/data \
+-v helloworld-data-of-nodejs:/server/data \
+-e "PORT=80" \
 -e "downloadURL=file:///server/data/helloworld.tar.gz" \
--p 81:8080 \
--m 1g \
+-p 81:80 \
+-m 128m \
+--dns 10.20.5.31 \
 --dns 114.114.114.114 \
-common-docker-starter:openjdk-8u191-jdk-alpine3.8
+common-node-alpine:8.15.1-alpine
